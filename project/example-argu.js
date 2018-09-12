b39c3d67c58d4e3142dff505742f6f8d8504df2d
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-09-12 16:46:50 +0200

    update example

--- project/example-argu.js
+++ project/example-argu.js
@@ -8,11 +8,6 @@ var hybridd = new Hybridd.Interface({http: require('http')});
 var base = 'waves';
 var symbol = 'waves.vote';
 
-openConnection({username:'3MNY6P42EYFPIVHW', password: '6AAOR4FMKF6VB5E2RE4ORUW7XPUUTSO7RNCZ2SOASWO47F6Y',hostname: 'http://wallet-uat.internetofcoins.org/api/'}
-               , (data) => { console.log(JSON.stringify(data)); }
-               , (error) => { console.error(error); }
-               , (progress) => { console.log(progress+'%'); }
-              );
 
 // data -> { username:username, password:password, hostname:hostname, successcb:successcb }
 function openConnection(data, successcb, errorcb,progresscb) {
@@ -28,13 +23,9 @@ function openConnection(data, successcb, errorcb,progresscb) {
                     );
 }
 
-function createTarget(successcb, errorcb) {
-  var hybriddTmp = new Hybridd.Interface({http: require('http')});
+function createTarget(data, successcb, errorcb, progresscb) {
   hybridd.sequential([
-    'init',
     {},'createAccount',
-    {host: 'http://wallet-uat.internetofcoins.org:1111'}, 'addHost',
-    data => {return {username:data.userid, password:data.passwd}}, 'session'
   ]
                      , (data) => { successcb(data); }
                      , (error) => { errorcb(error); }
@@ -42,14 +33,26 @@ function createTarget(successcb, errorcb) {
                     );
 }
 
-function sendVote(target, successcb, errorcb) {
+function sendVote(data, successcb, errorcb,progresscb) {
   var amount = 1;
   var symbol = 'waves.vote';
   hybridd.sequential([
-    {symbol: symbol, amount: Number(amount), target: target }, 'rawTransaction'
+    {symbol: symbol, amount: Number(amount), target: data }, 'rawTransaction'
   ]
                      , (data) => { successcb(data); }
                      , (error) => { errorcb(error); }
                      ,progresscb
                     );
 }
+
+
+hybridd.sequential([
+  {func:openConnection,data:{username:'3MNY6P42EYFPIVHW', password: '6AAOR4FMKF6VB5E2RE4ORUW7XPUUTSO7RNCZ2SOASWO47F6Y',hostname: 'http://wallet-uat.internetofcoins.org/api/'}},'callback',
+  {func:createTarget}, 'callback',
+  x => {console.log(x); return x;},
+  {func:sendVote,data:'3PHU3ibHUL311nfe3NR49MymBYX8iXffJEa'},'callback'
+]
+  , (data) => { console.log(JSON.stringify(data)); }
+               , (error) => { console.error(error); }
+               , (progress) => { console.log(progress+'%'); }
+                  );
